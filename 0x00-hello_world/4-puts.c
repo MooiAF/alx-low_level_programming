6253d9980e2f00b4664bb89197d65dcdc48b1162
root <mooi3dpheto@icloud.com>
2022-09-08 17:24:43 -0700

    fixed error

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,21 @@
-#!/bin/bash
-#include <stdio.h>
-
-/**
- * main - print the string in the put function
- *
- * description: using the main function
- * this program prints "programming is like  building a multilingual puzzle
- * return 0
- */
+#include <stdio.h>                                                                                                                                                                     
+                                                                                                                                                                                       
+/**                                                                                                                                                                                    
+* main - print the string in the put function                                                                                                                                          
+*                                                                                                                                                                                      
+*Description: using the main function                                                                                                                                                  
+root@3bbdcee21f70:~/alx-low_level_programming/0x00-hello_world# rm 4-puts.c                                                                                                            
+root@3bbdcee21f70:~/alx-low_level_programming/0x00-hello_world# cat > 4-puts.c                                                                                                         
+#include <stdio.h>                                                                                                                                                                     
+                                                                                                                                                                                       
+/**                                                                                                                                                                                    
+* main - print the string in the put function                                                                                                                                          
+*                                                                                                                                                                                      
+* Description: using main function                                                                                                                                                     
+* this program prints "programming is like building a multilingual puzzle
+* Return: 0
+*/
 int main(void)
-{
-	puts("\"programming is like building a multilingual puzzle");
-        return	(0);
-}	
+{   
+        puts("/"Programming is like building a multilingual puzzle");
+        return (0);
