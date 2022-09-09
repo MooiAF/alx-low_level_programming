8271559466bc62683131ec6673b4f0caf2a84d7b
root <mooi3dpheto@icloud.com>
2022-09-08 17:49:33 -0700

    fixed error

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,21 +1,13 @@
 #include <stdio.h>                                                                                                                                                                     
                                                                                                                                                                                        
 /**                                                                                                                                                                                    
-* main - print the string in the put function                                                                                                                                          
-*                                                                                                                                                                                      
-*Description: using the main function                                                                                                                                                  
-root@3bbdcee21f70:~/alx-low_level_programming/0x00-hello_world# rm 4-puts.c                                                                                                            
-root@3bbdcee21f70:~/alx-low_level_programming/0x00-hello_world# cat > 4-puts.c                                                                                                         
-#include <stdio.h>                                                                                                                                                                     
-                                                                                                                                                                                       
-/**                                                                                                                                                                                    
-* main - print the string in the put function                                                                                                                                          
-*                                                                                                                                                                                      
-* Description: using main function                                                                                                                                                     
-* this program prints "programming is like building a multilingual puzzle
-* Return: 0
-*/
-int main(void)
-{   
-        puts("/"Programming is like building a multilingual puzzle");
-        return (0);
+ * main - Printing                                                                                                                                                                     
+ *                                                                                                                                                                                     
+ * Return: Always 0 (Succesful)                                                                                                                                                        
+ */                                                                                                                                                                                    
+int main(void)                                                                                                                                                                         
+{                                                                                                                                                                                      
+	                                                                                                                                                                                       
+       puts("\"Programming is like building a multilingual puzzle");                                                                                                                          return (0);
+
+}	  
